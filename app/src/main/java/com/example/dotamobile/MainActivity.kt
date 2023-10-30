1d4d0abd073f9b9be191642c71061a0350f184c4
Alexander Safronov <sanya.safron2003@gmail.com>
2023-10-30 19:05:53 +0600

    fixed xmls and deleted tests

--- app/src/main/java/com/example/dotamobile/MainActivity.kt
+++ app/src/main/java/com/example/dotamobile/MainActivity.kt
@@ -1,20 +1,10 @@
 package com.example.dotamobile
 
+import MainScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
-import com.example.dotamobile.ui.theme.AppTheme
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
