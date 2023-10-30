ba9ac211e5801aa93c11a35bec0c9a1ea85ed454
Alexander Safronov <sanya.safron2003@gmail.com>
2023-10-30 19:02:56 +0600

    added AppTheme with TODO's and font agregator

--- app/src/main/java/com/example/dotamobile/ui/theme/Theme.kt
+++ app/src/main/java/com/example/dotamobile/ui/theme/Theme.kt
@@ -1,24 +1,12 @@
 package com.example.dotamobile.ui.theme
 
-import android.app.Activity
-import android.os.Build
+import FontFamily
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.core.view.WindowCompat
-import androidx.compose.material.Text
-import androidx.compose.ui.text.PlatformTextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.sp
 
@@ -26,6 +14,10 @@ object AppTheme {
 
     object BgColors {
 
+        val primary: Color
+            get() {
+                TODO()
+            }
     }
 
     object ButtonColors {
@@ -34,20 +26,36 @@ object AppTheme {
 
     object TextColors {
 
+        val secondary: Color
+            get() {
+                TODO()
+            }
     }
 
     object TextStyle {
+        val Regular_12_19: TextStyle
+            get() {
+                TODO()
+            }
         val Bold_48
-        @Composable
-        get() = TextStyle(
-            fontFamily = FontFamily,
-            fontWeight = FontWeight.Bold,
-            fontSize = 48.sp,
-            platformStyle = PlatformTextStyle(includeFontPadding = false),
-            lineHeightStyle = LineHeightStyle(
-                LineHeightStyle.Alignment.Proportional,
-                LineHeightStyle.Trim.None
-            )
-        )
+            @Composable
+            get() =
+                TextStyle(
+                    fontFamily = FontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 48.sp,
+                    platformStyle = PlatformTextStyle(includeFontPadding = false),
+                    lineHeightStyle = LineHeightStyle(
+                        LineHeightStyle.Alignment.Proportional,
+                        LineHeightStyle.Trim.None
+                    ),
+                )
     }
+}
+
+@Composable
+fun AppTheme(darkTheme: Boolean = isSystemInDarkTheme(),
+             content: @Composable () -> Unit
+) {
+
 }
