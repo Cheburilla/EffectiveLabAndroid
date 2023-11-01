5dad76ba00471063ba1803d6f7771e616933cd5f
Alexander Safronov <sanya.safron2003@gmail.com>
2023-11-02 03:34:25 +0600

    it were another project problems

--- app/src/main/java/com/example/dotamobile/ui/theme/fonts.kt
+++
@@ -1,9 +0,0 @@
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import com.example.dotamobile.R
-
-internal val FontFamily = FontFamily(
-    Font(R.font.sk_modernist_regular_font),
-    Font(R.font.sk_modernist_mono_font, weight = androidx.compose.ui.text.font.FontWeight.Companion.Medium),
-    Font(R.font.sk_modernist_bold_font, weight = androidx.compose.ui.text.font.FontWeight.Companion.Bold),
-)
